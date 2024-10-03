b7d29f04fe394739e8e7ba65417afd166c1b74c5
dough10 <doughten.jimi@gmail.com>
2024-10-02 19:43:26 -0500

    -e was output to console as well

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set
 version="0.2"
 
 BLACK='\033[0;30m'
@@ -14,28 +14,28 @@ YELLOW='\033[1;33m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-echo -e "${PURPLE} _____    ______   _    _   ______   _    _  10${NC}";
-echo -e "${PURPLE}| | \ \  / |  | \ | |  | | | | ____ | |  | |   ${NC}";
-echo -e "${PURPLE}| |  | | | |  | | | |  | | | |  | | | |--| |   ${NC}";
-echo -e "${PURPLE}|_|_/_/  \_|__|_/ \_|__|_| |_|__|_| |_|  |_|   ${NC}";
-echo -e "${WHITE}Clock Installer v:${NC}${GREEN}${version}${NC}";
+echo "${PURPLE} _____    ______   _    _   ______   _    _  10${NC}";
+echo "${PURPLE}| | \ \  / |  | \ | |  | | | | ____ | |  | |   ${NC}";
+echo "${PURPLE}| |  | | | |  | | | |  | | | |  | | | |--| |   ${NC}";
+echo "${PURPLE}|_|_/_/  \_|__|_/ \_|__|_| |_|__|_| |_|  |_|   ${NC}";
+echo "${WHITE}Clock Installer v:${NC}${GREEN}${version}${NC}";
 echo ""
 
-echo -e "${YELLOW}Install Python3 & PIP${NC}"
+echo "${YELLOW}Install Python3 & PIP${NC}"
 sudo apt update && sudo apt install python3 python3-pip python3.12-venv -y
-echo -e "${GREEN}Python3 & PIP Installed${NC}"
+echo "${GREEN}Python3 & PIP Installed${NC}"
 
 
-echo -e "${YELLOW}Setup virtual enviroment${NC}"
+echo "${YELLOW}Setup virtual enviroment${NC}"
 python3 -m venv ~/.venv
-echo -e "${GREEN}~/.venv created${NC}"
+echo "${GREEN}~/.venv created${NC}"
 
 
-echo -e "${YELLOW}Installing python modules${NC}"
+echo "${YELLOW}Installing python modules${NC}"
 ~/.venv/bin/python3 -m pip install -r requirments.txt
-echo -e "${GREEN}Python modules Installed${NC}"
+echo "${GREEN}Python modules Installed${NC}"
 
 
-echo -e "${YELLOW}Adding clock command to ~/.bashrc${NC}"
+echo "${YELLOW}Adding clock command to ~/.bashrc${NC}"
 echo '~/.venv/bin/python3 ~/clock/clock.py' >> ~/.bashrc
-echo -e "${GREEN}Clock command added to ~/.bashrc${NC}"
+echo "${GREEN}Clock command added to ~/.bashrc${NC}"
